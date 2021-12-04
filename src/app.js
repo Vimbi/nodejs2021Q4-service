df1bd5350f4730db3b4833f7db7965912d6ec165
Vimbi <i.am.vimbi@gmail.com>
2021-12-04 21:29:29 +0300

    feat: change package.json

--- src/app.js
+++ src/app.js
@@ -15,9 +15,6 @@ fastify.register(require('./resources/users/user.router'));
 fastify.register(require('./resources/boards/board.router'));
 fastify.register(require('./resources/tasks/task.router'));
 
-// TODO: https://discord.com/channels/755676888680366081/755860337059823667/915298425002684417 обнови темплейт
-// https://stackoverflow.com/questions/56577184/github-pull-changes-from-a-template-repository
-
 const start = async () => {
   try {
     await fastify.listen(PORT)
@@ -28,36 +25,5 @@ const start = async () => {
 }
 
 start();
-// const app = express();
-// const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
-
-// fastify.register(require('fastify-swagger'), {
-//   mode: 'static',
-//   specification: {
-//     path: '../doc/api.yaml',
-//     baseDir: '/path/to/external/spec/files/location',
-//   },
-// })
-
-// fastify.ready(err => {
-//   if (err) throw err
-//   fastify.swagger()
-// })
-
-// app.use(express.json());
-// app.get
-// app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
-// app.use('/', (req, res, next) => {
-//   if (req.originalUrl === '/') {
-//     res.send('Service is running!');
-//     return;
-//   }
-//   next();
-// });
-
-// app.use('/users', userRouter);
-
-// module.exports = app;
 
 
